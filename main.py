022899cb8e0587bd39cef73fc2ef60423e41e93b
Paulina <paulinasukure@gmail.com>
2026-01-05 15:56:46 +0200

    player can move in all directions

--- main.py
+++ main.py
@@ -8,24 +8,33 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
-
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
